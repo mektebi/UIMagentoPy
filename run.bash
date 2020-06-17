07444a223de0bd5e1c1bdbc362a9ed6aaa381d47
mektebi <mhdmaktabi@gmail.com>
2020-06-17 05:26:11 +0300

    fixed run bash

--- run.bash
+++ run.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=. pipenv run pytest -n 4
+PATH=${PATH}:/usr/local/bin
 
+pip3.8 install -r requirements.txt
 
+docker-compose up -d
 
-
+PYTHONPATH=. python3.8 -m pytest
